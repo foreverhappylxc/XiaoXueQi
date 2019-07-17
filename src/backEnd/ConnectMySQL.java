64564a4753406622e937cf8a02159d39dc7391c6
zjhlxc <1771583929@qq.com>
2019-07-17 09:08:39 +0800

    Test Connect

---
+++ src/backEnd/ConnectMySQL.java
@@ -0,0 +1,19 @@
+package backEnd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectMySQL {
+	public Connection conn;
+	
+	public ConnectMySQL()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://10.132.48.225:3306/usi?useSSL=false&serverTimezone=GMT"
+					, "root", "zxc123__");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
